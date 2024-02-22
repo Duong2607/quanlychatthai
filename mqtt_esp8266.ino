eb19a0a7172f8fd955f69eeb94221e02bf6e5b7e
Duong2607 <120171585+Duong2607@users.noreply.github.com>
2024-02-22 13:50:09 +0700

    Update mqtt_esp8266.ino

--- mqtt_esp8266.ino
+++ mqtt_esp8266.ino
@@ -1,31 +1,21 @@
-
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+const char* ssid = ""; // Ten wifi
+const char* password = ""; // Mat khau wifi
+const char* mqtt_server = "broker.hivemq.com"; // ip broker
 
-
-<<<<<<< HEAD:mqtt_esp8266/mqtt_esp8266.ino
-const char* ssid = "....";         // Ten wifi
-const char* password = "....";     // Mat khau wifi
-const char* mqtt_server = "....";  // ip local broker
-=======
-const char* ssid = "..."; // Ten wifi
-const char* password = "..."; // Mat khau wifi
-const char* mqtt_server = "..."; // ip broker
->>>>>>> 692236132a1330a75c4bd2249d3df7ea7be3d73d:mqtt_esp8266.ino
-
-const int trigPin = D7;
-const int echoPin = D8;
+const int trigPin = D7; 
+const int echoPin = D8; 
 
 long duration;
 int distance;
-int lastDistance;  // bien luu tru khoang cach truoc do
+ int lastDistance; // bien luu tru khoang cach truoc do
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE (50)
+#define MSG_BUFFER_SIZE	(50)
 char msg[MSG_BUFFER_SIZE];
 char ip[20];
 int value = 0;
@@ -37,8 +27,8 @@ void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
-    WiFi.mode(WIFI_STA);
+
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -62,17 +52,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);  // Turn the LED on (Note that LOW is the voltage level
-    Serial.println("LED ON");
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-    Serial.println("LED OFF");
-  }
+  snprintf (msg, MSG_BUFFER_SIZE, "%s",ip);
+  Serial.println(msg);
+  client.publish("connecting", msg);
 }
 
 void reconnect() {
@@ -88,7 +70,8 @@ void reconnect() {
       //truyen tin len kenh topic dulieu
       client.publish("dulieu", "hello world");
       // ... and resubscribe
-      client.subscribe("inTopic");
+      // client.subscribe("inTopic");
+      client.subscribe("CheckConnect");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -100,9 +83,9 @@ void reconnect() {
 }
 
 void setup() {
-  pinMode(trigPin, OUTPUT);      // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT);       // Sets the echoPin as an Input
-  pinMode(BUILTIN_LED, OUTPUT);  // Initialize the BUILTIN_LED pin as an output
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
@@ -110,52 +93,46 @@ void setup() {
 }
 
 void loop() {
-
-
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+    // Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+//luu ip vao bien ip de dinh danh cho esp
+sprintf(ip, "%d.%d.%d.%d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3]);
+  
+//kiem tra trang thai thay doi khong neu khoang cach thanh doi giua 20cm thi gui tin nhan len topic 
 
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-
-  // Calculating the distance
-  distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  // IPAddress ip = WiFi.localIP();
-  //luu ip vao bien ip de dinh danh cho esp
-  sprintf(ip, "%d.%d.%d.%d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3]);
-
-  //kiem tra trang thai thay doi khong neu khoang cach thanh doi giua 20cm thi gui tin nhan len topic
-
-  if (lastDistance < 20) {
-    if (distance > 20) {
-      snprintf(msg, MSG_BUFFER_SIZE, "%s#%ld", ip, distance);
-      client.publish("dulieu", msg);
-    }
+if(lastDistance<9) {
+  if(distance>9) {
+    snprintf (msg, MSG_BUFFER_SIZE, "%s#%ld",ip, distance);
+    client.publish("dulieu", msg);
   }
-  if (lastDistance > 20) {
-    if (distance < 20) {
-      snprintf(msg, MSG_BUFFER_SIZE, "%s#%ld", ip, distance);
-      client.publish("dulieu", msg);
-    }
+}
+if(lastDistance>9) {
+  if(distance<9) {
+    snprintf (msg, MSG_BUFFER_SIZE, "%s#%ld",ip, distance);
+    client.publish("dulieu", msg);
   }
+}
 
 
 
-  Serial.print("Distance: ");
-  Serial.println(distance);
+Serial.print("Distance: ");
+Serial.println(distance);
 
-  Serial.println("IP address: ");
-  lastDistance = distance;
-  delay(2000);
+Serial.println("IP address: ");
+lastDistance = distance;
+delay(2000);
+
 }
